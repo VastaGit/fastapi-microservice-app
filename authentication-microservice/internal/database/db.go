b41f74999df6557078a76e72bffa267499ca3112
askat <askat@gmail.com>
2024-11-01 16:57:50 +0200

    Initialize GORM with SQLite database support
    Introduce a new `InitDatabase` function to set up the SQLite database using GORM, facilitating easier and more efficient database management. This involves opening a connection to `users.db`, handling potential connection errors, and auto-migrating the User model for seamless schema updates. This enhancement streamlines database initialization and aligns with project requirements.

---
+++ authentication-microservice/internal/database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"database/sql"
+	models "gin-authentication/internal/models"
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	_ "modernc.org/sqlite"
+)
+
+func InitDatabase() *gorm.DB {
+	sqlDB, err := sql.Open("sqlite", "users.db")
+	if err != nil {
+		log.Fatal("Failed to open database: ", err)
+	}
+
+	// Use GORM to connect to the existing sql.DB
+	db, err := gorm.Open(sqlite.New(sqlite.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	// Auto-migrate the User model
+	if err := db.AutoMigrate(&models.User{}); err != nil {
+		log.Fatal("Failed to migrate database: ", err)
+	}
+
+	return db
+}
