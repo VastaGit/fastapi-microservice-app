de17b246d20b534b88a50d84f20fc9b3a739febd
askat <askat@gmail.com>
2024-11-13 13:54:38 +0200

    Enhance code readability and consistency
    Removed unused `HTTPException` import, improving code clarity by eliminating unnecessary components. Adjusted the return formatting of product-related functions to enhance readability. Ensured consistent spacing in Redis connection parameters, aligning with code style standards. These changes collectively improve maintainability and readability.

--- inventory-microservice/micro-service-app/main.py
+++ inventory-microservice/micro-service-app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 from pydantic import BaseModel
@@ -20,10 +20,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 redis = get_redis_connection(
-    host = redis_host,
-    port = int(redis_port),
-    password = redis_passport,
-    decode_responses = True,
+    host=redis_host,
+    port=int(redis_port),
+    password=redis_passport,
+    decode_responses=True,
 )
 
 
@@ -52,25 +52,37 @@ class ProductRead(BaseModel):
 # @app.get("/products", response_model=list[ProductRead])
 # async def get_products():
 #     products = Product.find().all()
-#     return [ProductRead(id=prod.pk, name=prod.name, price=prod.price, quantity_available=prod.quantity_available) for prod in products]
+#     return [ProductRead(
+    # id=prod.pk, name=prod.name, price=prod.price,
+    # quantity_available=prod.quantity_available) for prod in products
+    # ]
 
 @app.get("/products", response_model=list[ProductRead])
 async def get_products():
     product_pks = Product.all_pks()
     products = [Product.get(pk) for pk in product_pks]
-    return [ProductRead(id=prod.pk, name=prod.name, price=prod.price, quantity_available=prod.quantity_available) for prod in products]
+    return [ProductRead(
+        id=prod.pk, name=prod.name, price=prod.price,
+        quantity_available=prod.quantity_available) for prod in products
+    ]
 
 
 @app.post("/products", response_model=ProductRead)
 async def create_product(product: ProductCreate):
     prod = Product(**product.model_dump())
     prod.save()
-    return ProductRead(id=prod.pk, name=prod.name, price=prod.price, quantity_available=prod.quantity_available)
+    return ProductRead(
+        id=prod.pk, name=prod.name, price=prod.price,
+        quantity_available=prod.quantity_available)
+
 
 @app.get("/products/{pk}", response_model=ProductRead)
 async def get_product(pk: str):
     product = Product.get(pk)
-    return ProductRead(id=product.pk, name=product.name, price=product.price, quantity_available=product.quantity_available)
+    return ProductRead(
+        id=product.pk, name=product.name, price=product.price,
+        quantity_available=product.quantity_available)
+
 
 @app.delete("/products/{pk}")
 async def delete_product(pk: str):
