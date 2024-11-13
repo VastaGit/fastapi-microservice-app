19d007332dbfc1d5ad0afd22d7babee5fa6a584c
askat <askat@gmail.com>
2024-11-13 13:54:26 +0200

    Add UUID as primary key for User model
    Switched to using UUIDs as primary keys for the User model to enhance data uniqueness and avoid potential key conflicts. Integrated automatic UUID generation before record creation. This change improves scalability and data integrity across distributed systems.

--- authentication-microservice/internal/models/models.go
+++ authentication-microservice/internal/models/models.go
@@ -1,12 +1,19 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"uniqueIndex" json:"username"`
-	Email    string `gorm:"uniqueIndex" json:"email"`
-	Password string `json:"password,omitempty"` // Omitting password in JSON responses
+	ID       uuid.UUID `gorm:"type:uuid;primary_key;" json:"id"`
+	Username string    `gorm:"uniqueIndex" json:"username"`
+	Email    string    `gorm:"uniqueIndex" json:"email"`
+	Password string    `json:"password,omitempty"` // Omitting password in JSON responses
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New()
+	return
 }
